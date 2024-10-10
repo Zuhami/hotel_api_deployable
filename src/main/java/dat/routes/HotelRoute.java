7cda3e1702bae1d5966a7534bd30c6b6b0205dee
Jon Bertelsen <jobe@cphbusiness.dk>
2024-10-10 11:47:07 +0200

    change order of routes for populate

--- src/main/java/dat/routes/HotelRoute.java
+++ src/main/java/dat/routes/HotelRoute.java
@@ -13,12 +13,12 @@ public class HotelRoute {
     protected EndpointGroup getRoutes() {
 
         return () -> {
+            get("/populate", hotelController::populate);
             post("/", hotelController::create, Role.USER);
             get("/", hotelController::readAll);
             get("/{id}", hotelController::read);
             put("/{id}", hotelController::update);
             delete("/{id}", hotelController::delete);
-            get("/populate", hotelController::populate);
         };
     }
 }
